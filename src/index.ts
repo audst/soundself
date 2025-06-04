760a46eb14348eaef531968f45cbdd25e523adc9
Audrey Tsung <audreytsung2003@gmail.com>
2025-06-04 14:08:38 -0700

    working basic spotify api ex with search

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import SpotifyWebApi from 'spotify-web-api-node';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
 });
+
+async function run() {
+  try {
+   const token = await spotifyApi.clientCredentialsGrant();
+    spotifyApi.setAccessToken(token.body.access_token);
+
+    const result = await spotifyApi.getTrack('3n3Ppam7vgaVa1iaRUc9Lp');
+    console.log('🎵 track information:', result.body);
+  } catch (err) {
+    console.error('❌ error accessing Spotify API:', err);
+  }
+}
+
+run();
